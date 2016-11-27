d9d8eb7926d3378686227c2269da9a118ee674b5
Samantha Turley <SamiBrielle@Samanthas-MacBook-Air.local>
2016-11-27 07:30:21 -0600

    use abs path for shared lib

--- test/tests.py
+++ test/tests.py
@@ -1,7 +1,12 @@
 from ctypes import cdll
 from glob import glob
+from os import path
 
-lib = cdll.LoadLibrary(glob('*fstapi*')[0])
+globbed = glob('*fstapi*')
+if len(globbed) < 1:
+	raise Exception('cannot find shared lib')
+
+lib = cdll.LoadLibrary(path.abspath(globbed[0]))
 
 def sanity_test():
 	lib.test()
